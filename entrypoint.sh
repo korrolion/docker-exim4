56d1b1cc43909dc35870b4511ba33c0ee86137d2
Igor <Igor@MBP-Igor.lan>
2016-09-12 23:56:45 +0300

    dkim folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir /etc/exim4/dkim
+cp /dkim/* /etc/exim4/dkim/
+chgrp -R Debian-exim /etc/exim4/dkim
+chmod -R 640 /etc/exim4/dkim
 update-exim4.conf
 service exim4 start
 sleep infinity
